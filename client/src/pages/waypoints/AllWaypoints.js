1f4a8c6dc3e5ad6d34cc3e6db2771a9cbb18d085
Patrick Fortaleza <patrickfortaleza@gmail.com>
2021-04-21 15:36:58 -0700

    #56 scaffold react-router-dom navigation

---
+++ client/src/pages/waypoints/AllWaypoints.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AllWaypoints() {
+  return <div>AllWaypoints</div>;
+}
---
+++ client/src/pages/waypoints/WaypointCategories.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function WaypointCategories() {
+  return <div>WaypointCategories</div>;
+}
