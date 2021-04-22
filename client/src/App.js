f88b606841cc3bb90ffcb21bc30c8ea5158e8058
Patrick Fortaleza <patrickfortaleza@gmail.com>
2021-04-22 13:59:42 -0700

    #58 create header view component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import Navigation from "./navigation";
 import { AuthProvider } from "./context/AuthContext";
+import Header from "./components/Header";
 
 function App() {
   return (
     <AuthProvider>
+      <Header />
       <Navigation />
     </AuthProvider>
   );
