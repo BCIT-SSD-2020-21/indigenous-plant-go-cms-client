cf3b03d1c07fe90749bd390de688a2d4c3a792ea
apurugganan <apurugganan1@my.bcit.ca>
2021-04-23 09:20:26 -0700

    Added seeder.js

---
+++ server/seeder.js
@@ -0,0 +1,209 @@
+const {MongoClient, ObjectID} = require('mongodb')
+const bcrypt = require('bcryptjs')
+require('dotenv').config()
+
+const url = process.env.MONGO_DB_URL 
+const dbName = process.env.MONGO_DB_NAME 
+const client = new MongoClient(url, {useUnifiedTopology: true, useNewUrlParser: true})
+
+const seed = async () => {
+
+  await client.connect()
+  const db = client.db(dbName)
+
+  // I. List of  collections
+  const users = db.collection('users')
+  const plants = db.collection('plants')
+  const images = db.collection('images')
+  const audios = db.collection('audios')
+  const videos = db.collection('videos')
+  const locations = db.collection('locations')
+  const categories = db.collection('categories')
+  const tags = db.collection('tags')
+  const revision_history = db.collection('revision_history')
+  const custom_fields = db.collection('custom_fields')
+
+  // II. Delete Collections | Clean database
+  await users.deleteMany({})
+  await plants.deleteMany({})
+  await images.deleteMany({})
+  await audios.deleteMany({})
+  await videos.deleteMany({})
+  await locations.deleteMany({})
+  await categories.deleteMany({})
+  await tags.deleteMany({})
+  await revision_history.deleteMany({})
+  await custom_fields.deleteMany({})
+
+  // III. Insert Data to mongoDB
+  // 1. Insert Users
+  await users.insertMany([
+    {
+      "username" : "bob",    
+      "password" : "$2y$08$2pRScE9X7Jz8M0Ij7u5LCehFaBt5IePug7HGUBEuRONVH76zCvMtK",    
+      "email" : "bob@test.ca",    
+      "role" : "Admin"
+    },
+    {
+      "username" : "charlie",    
+      "password" : "$2y$08$2pRScE9X7Jz8M0Ij7u5LCehFaBt5IePug7HGUBEuRONVH76zCvMtK",    
+      "email" : "charlie@test.ca",    
+      "role" : "Manager"
+    }
+  ])
+
+  // 2. Insert Images
+  await images.insertMany([
+    {
+      "image_url": "s3.aws.indigenousplantgo.com/images/lavender-1.jpg",
+      "caption": "lavender"
+    },
+    {
+      "image_url": "s3.aws.indigenousplantgo.com/images/kinnikinnickl-1.jpg",
+      "caption": "kinnikinnick"
+    }
+  ])
+
+  // 3. Insert Videos
+  await videos.insertMany([
+    {
+      "video_url": "s3.aws.indigenousplantgo.com/video/lavender-bloom.mp4",
+      "caption": "A lavender flower blooming timelapse"
+    },
+    {
+      "video_url": "s3.aws.indigenousplantgo.com/video/kinnikinncik-bloom.mp4",
+      "caption": "A kinnikinnick flower blooming timelapse"
+    }
+  ])
+
+  // 4. Insert Audios
+  await audios.insertMany([
+    {
+      "image_url": "s3.aws.indigenousplantgo.com/audios/lavender-1.mp3",
+      "caption": "lavender"
+    },
+    {
+      "image_url": "s3.aws.indigenousplantgo.com/audios/kinnikinnick.mp3",
+      "caption": "kinnikinnick"
+    }
+  ])
+
+  // 5. Insert Locations
+  await locations.insertMany([
+    {
+      "location_name": "SW1",
+      "latitude": 49.251508,
+      "longitude": -123.003891,
+      "description": "infront of SW1"
+    },
+    {
+      "location_name": "SW2",
+      "latitude": 49.2516282,
+      "longitude": -123.0003427,
+      "description": "infront of SW2"
+    }
+  ])
+
+  // 6. Insert Categories
+  await categories.insertMany([
+    {
+      "category_name": "Caryophyllaceae"
+    },
+    {
+      "category_name": "Asteraceae"
+    }
+  ])
+
+  // 7. Insert Tags
+  await tags.insertMany([
+    {
+      "tag_name": "healing"
+    },
+    {
+      "tag_name": "luck"
+    }
+  ])
+
+  // 8. Insert Custom Fields
+  await custom_fields.insertMany([
+    {
+      "field_title" : "Water Intake",
+      "content" : "Daily"
+    },
+    {
+      "field_title" : "Color",
+      "content" : "Indigo"
+    }
+  ])
+
+  // 9. Insert Revision Histroy
+  const user1 = await users.findOne({username: "bob"})
+  const user2 = await users.findOne({username: "charlie"})
+
+  await revision_history.insertMany([
+    { 
+      "user": user1._id,
+      "date": "11/13/2020"
+    },
+    {
+      "user": user2._id,
+      "date": "10/1/2020"
+    }
+  ])
+
+  // 10. Insert Plants
+  const image1 = await images.findOne({"caption": "lavender"})
+  const image2 = await images.findOne({"caption": "kinnikinnick"})
+  const video1 = await videos.findOne({"video_url": "s3.aws.indigenousplantgo.com/video/lavender-bloom.mp4"})
+  const video2 = await videos.findOne({"video_url": "s3.aws.indigenousplantgo.com/video/kinnikinncik-bloom.mp4"})
+  const audio1 = await audios.findOne({"caption": "lavender"})
+  const audio2 = await audios.findOne({"caption": "kinnikinnick"})
+  const location1 = await locations.findOne({"location_name": "SW1"})
+  const location2 = await locations.findOne({"location_name": "SW2"})
+  const category1 = await categories.findOne({"category_name": "Caryophyllaceae"})
+  const category2 = await categories.findOne({"category_name": "Asteraceae"})
+  const tag1 = await tags.findOne({"tag_name": "healing"})
+  const tag2 = await tags.findOne({"tag_name": "luck"})
+  const custom_field1 = await custom_fields.findOne({"field_title" : "Water Intake"})
+  const custom_field2 = await custom_fields.findOne({"content" : "Daily"})
+  const revision_history1 = await revision_history.findOne({"user": user1._id})
+  const revision_history2 = await revision_history.findOne({"user": user2._id})
+  
+  await plants.insertMany([
+    {
+      "plant_name": "lavender",
+      "scientific_name": "lavandula",
+      "description": "Lavender is a perennial shrub with purple flowers that bloom during the summer",
+      "images" : [image1._id],
+      "videos" : [video1._id],
+      "audios" : [audio1._id],
+      "locations" : [location1._id],
+      "categories" : [category1._id],
+      "tags" : [tag1._id],
+      "custom_fields" : [custom_field1._id],
+      "revision_histories" : [revision_history1._id]
+    },
+    {
+      "plant_name": "kinnikinnick",
+      "scientific_name": "arctostaphylos uva-ursi",
+      "description": "Kinnikinnick is an evergreen ground cover. It has small white or pink flowers that typically bloom in May and June, and red berries later in the season. Kinnikinnick attracts butterflies and other pollinators. It’s a food source for wildlife all year round. ",
+      "images" : [image2._id],
+      "videos" : [video2._id],
+      "audios" : [audio2._id],
+      "locations" : [location2._id],
+      "categories" : [category2._id],
+      "tags" : [tag2._id],
+      "custom_fields" : [custom_field2._id],
+      "revision_histories" : [revision_history2._id]
+    }
+
+  ])
+}
+
+// IV. Run function
+;(async ()=> {
+  await seed()
+  console.log("Mongo Connected!!");
+  await client.close();
+}
+)()
