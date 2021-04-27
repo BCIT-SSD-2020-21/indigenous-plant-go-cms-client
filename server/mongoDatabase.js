e4f80cb44180fd3eb5db497c4025e393273a08ea
daveroen <daveroenb@gmail.com>
2021-04-27 10:39:42 -0700

    added controller for reset pass

--- server/mongoDatabase.js
+++ server/mongoDatabase.js
@@ -55,6 +55,108 @@ module.exports = async function() {
     return result
   }
 
+//this is for  checking if the user exist
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+      throw new Error("User does not exist");
+  }
+  let token = await Token.findOne({ userId: user._id });
+  if (token) { 
+        await token.deleteOne()
+  };
+
+
+  //sends plain token to the customer
+const JWT = require("jsonwebtoken");
+const User = require("../models/User.model");
+const Token = require("../models/Token.model");
+const sendEmail = require("../utils/email/sendEmail");
+const crypto = require("crypto");
+const bcrypt = require("bcrypt");
+
+const requestPasswordReset = async (email) => {
+
+  const user = await User.findOne({ email });
+
+  if (!user) throw new Error("User does not exist");
+  let token = await Token.findOne({ userId: user._id });
+  if (token) await token.deleteOne();
+  let resetToken = crypto.randomBytes(32).toString("hex");
+  const hash = await bcrypt.hash(resetToken, Number(bcryptSalt));
+
+  await new Token({
+    userId: user._id,
+    token: hash,
+    createdAt: Date.now(),
+  }).save();
+
+  const link = `${clientURL}/passwordReset?token=${resetToken}&id=${user._id}`;
+  sendEmail(user.email,"Password Reset Request",{name: user.name,link: link,},"./template/requestResetPassword.handlebars");
+  return link;
+};
+
+const resetPassword = async (userId, token, password) => {
+  let passwordResetToken = await Token.findOne({ userId });
+  if (!passwordResetToken) {
+    throw new Error("Invalid or expired password reset token");
+  }
+  const isValid = await bcrypt.compare(token, passwordResetToken.token);
+  if (!isValid) {
+    throw new Error("Invalid or expired password reset token");
+  }
+  const hash = await bcrypt.hash(password, Number(bcryptSalt));
+  await User.updateOne(
+    { _id: userId },
+    { $set: { password: hash } },
+    { new: true }
+  );
+  const user = await User.findById({ _id: userId });
+  sendEmail(
+    user.email,
+    "Password Reset Successfully",
+    {
+      name: user.name,
+    },
+    "./template/resetPassword.handlebars"
+  );
+  await passwordResetToken.deleteOne();
+  return true;
+};
+
+const {
+  signup,
+  requestPasswordReset,
+  resetPassword,
+} = require("../services/auth.service");
+
+const signUpController = async (req, res, next) => {
+  const signupService = await signup(req.body);
+  return res.json(signupService);
+};
+
+const resetPasswordRequestController = async (req, res, next) => {
+  const requestPasswordResetService = await requestPasswordReset(
+    req.body.email
+  );
+  return res.json(requestPasswordResetService);
+};
+
+const resetPasswordController = async (req, res, next) => {
+  const resetPasswordService = await resetPassword(
+    req.body.userId,
+    req.body.token,
+    req.body.password
+  );
+  return res.json(resetPasswordService);
+};
+
+module.exports = {
+  signUpController,
+  resetPasswordRequestController,
+  resetPasswordController,
+};
   //Get One user, use for login
   //Takes in email/username and password and find one user that match
   //POST /api/users/login
@@ -599,6 +701,9 @@ module.exports = async function() {
       }
     ]
   }
+
+
+
 
   return {
     //User
