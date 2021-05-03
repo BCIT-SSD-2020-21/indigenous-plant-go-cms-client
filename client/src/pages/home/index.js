ad9701a072fb6af51d0050e118a3ad5c6ef8b31f
apurugganan <apurugganan1@my.bcit.ca>
2021-05-03 09:16:35 -0700

    removed prev and next icon

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -66,7 +66,7 @@ export default function Home({action, method}) {
     <main>
       {/* HERO SECTION */}
       <div style={style.hero}>
-        <Carousel fade interval="8000" indicators={false}>
+        <Carousel fade interval="8000" indicators={false} controls = {false}>
           <Carousel.Item>
             <img className="carousel" style={style.image} src="/assets/images/hero.jpg" alt="Indigenous Initiatives and Partnerships Logo Red"/>
           </Carousel.Item>
