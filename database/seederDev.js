dd056aa0c84fbd5c0bbfc4acbb61be05b47960de
apurugganan <apurugganan1@my.bcit.ca>
2021-05-12 12:00:18 -0700

    Update

--- database/seederDev.js
+++ database/seederDev.js
@@ -3,7 +3,7 @@ const {MongoClient, ObjectID, Timestamp} = require('mongodb')
 require('dotenv').config()
 
 const url =  process.env.MONGO_DB_URL 
-const dbName = process.env.MONGO_DB_NAME 
+const dbName = process.env.MONGO_DB_NAME
 const client = new MongoClient(url, {useUnifiedTopology: true, useNewUrlParser: true})
 
 const seed = async () => {
