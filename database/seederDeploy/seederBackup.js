a6689536858b49ebbfa98f169b74afe7e4078dbd
apurugganan <apurugganan1@my.bcit.ca>
2021-05-12 20:57:54 -0700

    placeholder revision_history added seederBackup.js

--- database/seederDeploy/seederBackup.js
+++ database/seederDeploy/seederBackup.js
@@ -31,6 +31,7 @@ const seed = async () => {
   const tags = db.collection('tags')
   const waypoints = db.collection('waypoints')
   const plants = db.collection('plants')
+  const revisions = db.collection('revisions')
 
   // INSERT DATA TO MONGODB
   await users.insertMany(usersList)
@@ -40,6 +41,8 @@ const seed = async () => {
   await locations.insertMany(locationsList)
   await categories.insertMany(categoriesList)
   await tags.insertMany(tagsList)
+
+  
 
   // Insert Plants
   const lavenderImage1 = await images.findOne({"caption": "lavender"})
@@ -55,6 +58,24 @@ const seed = async () => {
   const lotOLocation1 = await locations.findOne({"location_name": "Lot O (general)"})
   const englishwalkLocation1 = await locations.findOne({"location_name": "English Walk (general)"})
   
+  const gatheringVideoPH = await videos.findOne({"caption": "The Indigenous Initiatives Gathering Place"})
+  const audio1 = await audios.findOne({"caption": "lavender"})
+  const category1 = await categories.findOne({"category_name": "traditional plants"})
+  const tag1 = await tags.findOne({"tag_name": "healing"})
+  const user1 = await users.findOne({"user_name": "bob"})
+  await revisions.insertMany([
+    { 
+      "user": user1._id,
+      "date": 1619651670890
+    },
+    {
+      "user": user1._id,
+      "date": 1619650526509
+    },
+    
+  ])
+  const revision_history1 = await revisions.findOne({"user": user1._id})
+
   await plants.insertMany([
     {
       "plant_name": "lavender",
@@ -67,179 +88,179 @@ const seed = async () => {
       "audio_files" : [],
       "categories" : [],
       "tags" : [],
-      "revision_history" : [],
+      "revision_history" : [revision_history1._id],
       "custom_fields" : []
     },
-    {
-      "plant_name": "kinnikinnick",
-      "scientific_name": "arctostaphylos uva-ursi",
-      "description": "Kinnikinnick is an evergreen ground cover. It has small white or pink flowers that typically bloom in May and June, and red berries later in the season. Kinnikinnick attracts butterflies and other pollinators. It’s a food source for wildlife all year round.",
-      "images" : [kinnikinnickImage1._id],
-      "locations" : [kinnikinnickLocation1._id],
-      "isPublish" : true,
-      "videos" : [],
-      "audio_files" : [],
-      "categories" : [],
-      "tags" : [],
-      "revision_history" : [],
-      "custom_fields" : []
-    },
-    {
-      "plant_name": "Coastal Strawberry",
-      "scientific_name": "Coastal Strawberry",
-      "description": "This evergreen ground cover has three leaves, and white flowers that bloom in the summer. The coastal strawberry attracts pollinators. It’s a food source for wildlife in the spring and summer.",
-      "locations" : [coastalLocation1._id],
-      "isPublish" : true,
-      "videos" : [],
-      "audio_files" : [],
-      "categories" : [],
-      "tags" : [],
-      "revision_history" : [],
-      "custom_fields" : []
-    },
-    {
-      "plant_name": "Salal",
-      "scientific_name": "Gaultheria shallon ",
-      "description": "Salal is a perennial evergreen shrub. Salal attracts pollinators and hummingbirds. It’s a food source for wildlife all year round.",   
-      "locations" : [salalLocation1._id],
-      "isPublish" : true,
-      "videos" : [],
-      "audio_files" : [],
-      "categories" : [],
-      "tags" : [],
-      "revision_history" : [],
-      "custom_fields" : []
-    },
-    {
-      "plant_name": "Snowberry",
-      "scientific_name": "Symphoricarpos albus",
-      "description": "These shrubs have white flowers in spring, and white berries through the fall and winter.",   
-      "locations" : [snowberryLocation1._id],
-      "isPublish" : true,
-      "videos" : [],
-      "audio_files" : [],
-      "categories" : [],
-      "tags" : [],
-      "revision_history" : [],
-      "custom_fields" : []
-    },
-    {
-      "plant_name": "Snow Fern" ,
-      "scientific_name": "Polystichum munitum",
-      "description": "Sword fern gets its name from its elongated blade-like fronds. The robust, handsome leaves can grow several feet long and the plant can have as many as a hundred leaves. This evergreen fern can be used to prevent erosion on slopes. Sword ferns are a food source for wildlife all year round.",   
-      "locations" : [swordfernLocation1._id],
-      "isPublish" : true,
-      "videos" : [],
-      "audio_files" : [],
-      "categories" : [],
-      "tags" : [],
-      "revision_history" : [],
-      "custom_fields" : []
-    },
-    {
-      "plant_name": "Western Red Cedar",
-      "scientific_name": "Thuja plicata",
-      "description": "Western Red Cedar is a large evergreen tree, they can grow to 120 – 150 feet. Western Red cedar are a food source for wildlife all year round. Because cedar is resistant to decay it was used for building long houses, canoes and other items. The bark can be shredded and used to weave.",   
-      "locations" : [westerncedarLocation._id, lotOLocation1._id],
-      "isPublish" : true,
-      "videos" : [],
-      "audio_files" : [],
-      "categories" : [],
-      "tags" : [],
-      "revision_history" : [],
-      "custom_fields" : []
-    },
-    {
-      "plant_name": "Stika Spruce",
-      "scientific_name": "Picea sitchensis",
-      "description": "The Sitka Spruce is a conifer tree that can grow to 180 – 225 feet tall. ",   
-      "locations" : [lotOLocation1._id],
-      "isPublish" : true,
-      "videos" : [],
-      "audio_files" : [],
-      "categories" : [],
-      "tags" : [],
-      "revision_history" : [],
-      "custom_fields" : []
-    },
-    {
-      "plant_name": "Big Maple Leaf",
-      "scientific_name": "Acer macrophyllum",
-      "description": "Big Leaf Maples typically grow to 30 – 75 feet, and may live to be 200 years old. It is a food source for wildlife. ",   
-      "locations" : [lotOLocation1._id],
-      "isPublish" : true,
-      "videos" : [],
-      "audio_files" : [],
-      "categories" : [],
-      "tags" : [],
-      "revision_history" : [],
-      "custom_fields" : []
-    },
-    {
-      "plant_name": "Salmonberry",
-      "scientific_name": "Rubus Spectabilis",
-      "description": "Salmonberry have pink flowers with five petals in April - May, and yellow-orange berries through May - July. The flowers are attractive to hummingbirds, and the plant is a food source for wildlife in summer.",   
-      "locations" : [lotOLocation1._id],
-      "isPublish" : true,
-      "videos" : [],
-      "audio_files" : [],
-      "categories" : [],
-      "tags" : [],
-      "revision_history" : [],
-      "custom_fields" : []
-    },
-    {
-      "plant_name": "Red Elderberry",
-      "scientific_name": "Sambucus racemosa",
-      "description": "Red Elderberry have white flowers in April - July, and bright red berries through July – August. Red Elderberry is used for erosion control. The plant is a food source for wildlife and pollinators.",   
-      "locations" : [lotOLocation1._id],
-      "isPublish" : true,
-      "videos" : [],
-      "audio_files" : [],
-      "categories" : [],
-      "tags" : [],
-      "revision_history" : [],
-      "custom_fields" : []
-    },
-    {
-      "plant_name": "Red-Osier Dogwood",
-      "scientific_name": "cornus sericea arctic",
-      "description": "Placeholder description",   
-      "locations" : [lotOLocation1._id, englishwalkLocation1._id],
-      "isPublish" : true,
-      "videos" : [],
-      "audio_files" : [],
-      "categories" : [],
-      "tags" : [],
-      "revision_history" : [],
-      "custom_fields" : []
-    },
-    {
-      "plant_name": "Thimbleberry",
-      "scientific_name": "Rubus parviflorus",
-      "description": "Thimbleberry have white flowers in May - June, and small red berries through July – September. The plant is a food source for wildlife and pollinators during summer.",   
-      "locations" : [lotOLocation1._id],
-      "isPublish" : true,
-      "videos" : [],
-      "audio_files" : [],
-      "categories" : [],
-      "tags" : [],
-      "revision_history" : [],
-      "custom_fields" : []
-    },
-    {
-      "plant_name": "Evergreen Huckleberry",
-      "scientific_name": "Vaccinium ovatum",
-      "description": "Evergreen Huckleberry have pinkish-white flowers in April - May, and purplish-black berries through August – September. The plant is a food source for wildlife and pollinators.",   
-      "locations" : [englishwalkLocation1],
-      "isPublish" : true,
-      "videos" : [],
-      "audio_files" : [],
-      "categories" : [],
-      "tags" : [],
-      "revision_history" : [],
-      "custom_fields" : []
-    },
+    // {
+    //   "plant_name": "kinnikinnick",
+    //   "scientific_name": "arctostaphylos uva-ursi",
+    //   "description": "Kinnikinnick is an evergreen ground cover. It has small white or pink flowers that typically bloom in May and June, and red berries later in the season. Kinnikinnick attracts butterflies and other pollinators. It’s a food source for wildlife all year round.",
+    //   "images" : [kinnikinnickImage1._id],
+    //   "locations" : [kinnikinnickLocation1._id],
+    //   "isPublish" : true,
+    //   "videos" : [],
+    //   "audio_files" : [],
+    //   "categories" : [],
+    //   "tags" : [],
+    //   "revision_history" : [],
+    //   "custom_fields" : []
+    // },
+    // {
+    //   "plant_name": "Coastal Strawberry",
+    //   "scientific_name": "Coastal Strawberry",
+    //   "description": "This evergreen ground cover has three leaves, and white flowers that bloom in the summer. The coastal strawberry attracts pollinators. It’s a food source for wildlife in the spring and summer.",
+    //   "locations" : [coastalLocation1._id],
+    //   "isPublish" : true,
+    //   "videos" : [],
+    //   "audio_files" : [],
+    //   "categories" : [],
+    //   "tags" : [],
+    //   "revision_history" : [],
+    //   "custom_fields" : []
+    // },
+    // {
+    //   "plant_name": "Salal",
+    //   "scientific_name": "Gaultheria shallon ",
+    //   "description": "Salal is a perennial evergreen shrub. Salal attracts pollinators and hummingbirds. It’s a food source for wildlife all year round.",   
+    //   "locations" : [salalLocation1._id],
+    //   "isPublish" : true,
+    //   "videos" : [],
+    //   "audio_files" : [],
+    //   "categories" : [],
+    //   "tags" : [],
+    //   "revision_history" : [],
+    //   "custom_fields" : []
+    // },
+    // {
+    //   "plant_name": "Snowberry",
+    //   "scientific_name": "Symphoricarpos albus",
+    //   "description": "These shrubs have white flowers in spring, and white berries through the fall and winter.",   
+    //   "locations" : [snowberryLocation1._id],
+    //   "isPublish" : true,
+    //   "videos" : [],
+    //   "audio_files" : [],
+    //   "categories" : [],
+    //   "tags" : [],
+    //   "revision_history" : [],
+    //   "custom_fields" : []
+    // },
+    // {
+    //   "plant_name": "Snow Fern" ,
+    //   "scientific_name": "Polystichum munitum",
+    //   "description": "Sword fern gets its name from its elongated blade-like fronds. The robust, handsome leaves can grow several feet long and the plant can have as many as a hundred leaves. This evergreen fern can be used to prevent erosion on slopes. Sword ferns are a food source for wildlife all year round.",   
+    //   "locations" : [swordfernLocation1._id],
+    //   "isPublish" : true,
+    //   "videos" : [],
+    //   "audio_files" : [],
+    //   "categories" : [],
+    //   "tags" : [],
+    //   "revision_history" : [],
+    //   "custom_fields" : []
+    // },
+    // {
+    //   "plant_name": "Western Red Cedar",
+    //   "scientific_name": "Thuja plicata",
+    //   "description": "Western Red Cedar is a large evergreen tree, they can grow to 120 – 150 feet. Western Red cedar are a food source for wildlife all year round. Because cedar is resistant to decay it was used for building long houses, canoes and other items. The bark can be shredded and used to weave.",   
+    //   "locations" : [westerncedarLocation._id, lotOLocation1._id],
+    //   "isPublish" : true,
+    //   "videos" : [],
+    //   "audio_files" : [],
+    //   "categories" : [],
+    //   "tags" : [],
+    //   "revision_history" : [],
+    //   "custom_fields" : []
+    // },
+    // {
+    //   "plant_name": "Stika Spruce",
+    //   "scientific_name": "Picea sitchensis",
+    //   "description": "The Sitka Spruce is a conifer tree that can grow to 180 – 225 feet tall. ",   
+    //   "locations" : [lotOLocation1._id],
+    //   "isPublish" : true,
+    //   "videos" : [],
+    //   "audio_files" : [],
+    //   "categories" : [],
+    //   "tags" : [],
+    //   "revision_history" : [],
+    //   "custom_fields" : []
+    // },
+    // {
+    //   "plant_name": "Big Maple Leaf",
+    //   "scientific_name": "Acer macrophyllum",
+    //   "description": "Big Leaf Maples typically grow to 30 – 75 feet, and may live to be 200 years old. It is a food source for wildlife. ",   
+    //   "locations" : [lotOLocation1._id],
+    //   "isPublish" : true,
+    //   "videos" : [],
+    //   "audio_files" : [],
+    //   "categories" : [],
+    //   "tags" : [],
+    //   "revision_history" : [],
+    //   "custom_fields" : []
+    // },
+    // {
+    //   "plant_name": "Salmonberry",
+    //   "scientific_name": "Rubus Spectabilis",
+    //   "description": "Salmonberry have pink flowers with five petals in April - May, and yellow-orange berries through May - July. The flowers are attractive to hummingbirds, and the plant is a food source for wildlife in summer.",   
+    //   "locations" : [lotOLocation1._id],
+    //   "isPublish" : true,
+    //   "videos" : [],
+    //   "audio_files" : [],
+    //   "categories" : [],
+    //   "tags" : [],
+    //   "revision_history" : [],
+    //   "custom_fields" : []
+    // },
+    // {
+    //   "plant_name": "Red Elderberry",
+    //   "scientific_name": "Sambucus racemosa",
+    //   "description": "Red Elderberry have white flowers in April - July, and bright red berries through July – August. Red Elderberry is used for erosion control. The plant is a food source for wildlife and pollinators.",   
+    //   "locations" : [lotOLocation1._id],
+    //   "isPublish" : true,
+    //   "videos" : [],
+    //   "audio_files" : [],
+    //   "categories" : [],
+    //   "tags" : [],
+    //   "revision_history" : [],
+    //   "custom_fields" : []
+    // },
+    // {
+    //   "plant_name": "Red-Osier Dogwood",
+    //   "scientific_name": "cornus sericea arctic",
+    //   "description": "Placeholder description",   
+    //   "locations" : [lotOLocation1._id, englishwalkLocation1._id],
+    //   "isPublish" : true,
+    //   "videos" : [],
+    //   "audio_files" : [],
+    //   "categories" : [],
+    //   "tags" : [],
+    //   "revision_history" : [],
+    //   "custom_fields" : []
+    // },
+    // {
+    //   "plant_name": "Thimbleberry",
+    //   "scientific_name": "Rubus parviflorus",
+    //   "description": "Thimbleberry have white flowers in May - June, and small red berries through July – September. The plant is a food source for wildlife and pollinators during summer.",   
+    //   "locations" : [lotOLocation1._id],
+    //   "isPublish" : true,
+    //   "videos" : [],
+    //   "audio_files" : [],
+    //   "categories" : [],
+    //   "tags" : [],
+    //   "revision_history" : [],
+    //   "custom_fields" : []
+    // },
+    // {
+    //   "plant_name": "Evergreen Huckleberry",
+    //   "scientific_name": "Vaccinium ovatum",
+    //   "description": "Evergreen Huckleberry have pinkish-white flowers in April - May, and purplish-black berries through August – September. The plant is a food source for wildlife and pollinators.",   
+    //   "locations" : [englishwalkLocation1._id],
+    //   "isPublish" : true,
+    //   "videos" : [],
+    //   "audio_files" : [],
+    //   "categories" : [],
+    //   "tags" : [],
+    //   "revision_history" : [],
+    //   "custom_fields" : []
+    // },
   ])
 
 
